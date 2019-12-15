7b34a65a7b97a612172ff696e2b56c2daab17ad6
Chi Joshua <37910718+chijosh@users.noreply.github.com>
2019-12-15 23:14:14 +0200

    removed files from root folder

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,8 +6,32 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //  BUILD QUERY
+    //1A) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach(el => delete queryObj[el]);
 
+    //1B) Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    //2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      console.log(sortBy);
+
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    //EXECUTE
+    const tours = await query;
+
+    // SENT RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
